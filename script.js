7c3fa665b46c383f188e35c66415d58d4985993f
Sveta <svet.lukyantseva@gmail.com>
2022-05-03 22:04:44 +0300

    init: start js4-permutations task

---
+++ script.js
@@ -0,0 +1,6 @@
+// Implement program to print all permutations of a given string
+// Example  
+// Input: ABC
+// Output: ABC, ACB, BAC, BCA, CAB, CBA
+
+// *Print all permutations in sorted (lexicographic) order
