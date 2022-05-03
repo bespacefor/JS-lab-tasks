40c689d157c7be2f55f98dedce8c2d4dd9d0b12a
Sveta <svet.lukyantseva@gmail.com>
2022-05-04 01:09:18 +0300

    feat: add js4-permutations task

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-// Implement program to print all permutations of a given string
-// Example  
-// Input: ABC
-// Output: ABC, ACB, BAC, BCA, CAB, CBA
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+const button = document.getElementById('button');
 
-// *Print all permutations in sorted (lexicographic) order
+function permutation(str) {
+    if (str.length <= 2) {
+        return str.length === 2 ? [str, str[1] + str[0]] : [str]
+    }
+    return str
+      .split('')
+      .reduce(
+        (acc, letter, i) =>
+          acc.concat(permutation(str.slice(0, i) + str.slice(i + 1)).map(val => letter + val)),
+        []
+    );
+}
+
+function print(str) {
+    output.value = Array.from(
+        new Set(permutation(str))
+    ).sort().join(', ');
+}
+
+button.addEventListener('click', () => {
+    permutation(input.value);
+    print(input.value);
+});
