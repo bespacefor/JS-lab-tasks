f1d4dba0dfe507d1ed30853525f1f8ae6efab122
Sveta <svet.lukyantseva@gmail.com>
2022-05-18 12:31:16 +0300

    init: start js6-sorting-table task

---
+++ JS6 Sorting table/script.js
@@ -0,0 +1,5 @@
+//Add to html page table with header and some rows
+//Add to header event listener to implement sorting functionality across the table data(alphabetical)
+//Add to cells user interaction feature(click and edit text)
+//Table in whole should be created with JS and appended to the document body on DOMContentLoaded event*
+
